55a89a3cd3aaec2c10c82f398ad6033a5b9be545
Ryan Schachte <rschacht@asu.edu>
2018-06-12 06:31:23 -0700

    Palindrome permutation

--- src/main/java/ArraysAndStrings/PalindromePermutation.java
+++ src/main/java/ArraysAndStrings/PalindromePermutation.java
@@ -1,9 +1,10 @@
 package ArraysAndStrings;
 
 import com.google.common.collect.ImmutableList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class PalindromePermutation {
@@ -18,37 +19,73 @@ public class PalindromePermutation {
         "cato tac"
     );
 
-    for (String testItem : testData) {
-      permute(testItem);
-      if (palindromeTracker.size() > 0) {
-        System.out.println(testItem + " is true");
-      } else {
-        System.out.println(testItem + " is false");
-      }
-      palindromeTracker = new HashSet<>();
+    for (String item : testData) {
+      System.out.println(item + " is " + checkEvenNumberedCounts(item));
     }
   }
 
-  public static void permute(String inputString) {
-    permuteHelper(inputString, "");
+  public static boolean isEven(String input) {
+    return input.length() % 2 == 0;
   }
 
-  public static void permuteHelper(String inputString, String chosen) {
-    if (inputString.isEmpty()) {
-      if (isPalindrome(chosen)) {
-        palindromeTracker.add(chosen);
+  public static Map<Character, Integer> populateMap(String input)  {
+    Map<Character, Integer> charCounts = new HashMap<>();
+    for (Character c : input.toCharArray()) {
+      charCounts.put(c, 0);
+    }
+
+    return charCounts;
+  }
+
+  public static boolean checkEvenNumberedCounts(String input) {
+    Map<Character, Integer> charCounts = populateMap(input);
+    int oddCount = 0;
+
+    for (Character c : input.toCharArray()) {
+      int currentVal = charCounts.get(c) + 1;
+      charCounts.put(c, currentVal);
+    }
+
+    for (Character c : input.toCharArray()) {
+      int currentVal = charCounts.get(c);
+      if (currentVal % 2 != 0) {
+        oddCount += 1;
       }
+    }
+
+    if (isEven(input) && oddCount != 0) {
+      return false;
+    } else if (isEven(input) && oddCount == 0) {
+      return true;
+    } else if (!isEven(input) && oddCount <= 1) {
+      return true;
     } else {
-      for (int i = 0; i < inputString.length(); i++) {
-        String rem = inputString.substring(0, i) + inputString.substring(i+1);
-        permuteHelper(rem, chosen + inputString.charAt(i));
-      }
+      return false;
     }
   }
 
-  /** Checks if the input string is a palindrome */
-  private static boolean isPalindrome(String potentialPalindrome) {
-    potentialPalindrome = potentialPalindrome.replace(" ", "");
-    return potentialPalindrome.equals(new StringBuilder(potentialPalindrome).reverse().toString());
-  }
+
+
+//  public static void permute(String inputString) {
+//    permuteHelper(inputString, "");
+//  }
+//
+//  public static void permuteHelper(String inputString, String chosen) {
+//    if (inputString.isEmpty()) {
+//      if (isPalindrome(chosen)) {
+//        palindromeTracker.add(chosen);
+//      }
+//    } else {
+//      for (int i = 0; i < inputString.length(); i++) {
+//        String rem = inputString.substring(0, i) + inputString.substring(i+1);
+//        permuteHelper(rem, chosen + inputString.charAt(i));
+//      }
+//    }
+//  }
+//
+//  /** Checks if the input string is a palindrome */
+//  private static boolean isPalindrome(String potentialPalindrome) {
+//    potentialPalindrome = potentialPalindrome.replace(" ", "");
+//    return potentialPalindrome.equals(new StringBuilder(potentialPalindrome).reverse().toString());
+//  }
 }
