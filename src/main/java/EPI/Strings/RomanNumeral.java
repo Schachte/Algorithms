be38033c53a2a39425d6f99e5fd567546887b2a5
Ryan Schachte <rschacht@asu.edu>
2018-07-09 07:05:55 -0700

    Begin thinking of roman numeral program

---
+++ src/main/java/EPI/Strings/RomanNumeral.java
@@ -0,0 +1,24 @@
+package EPI.Strings;
+
+/**
+ * Programatically convert roman numeral into associated integer form
+ * Must handle the 6 exception cases
+ *
+ * I = 1
+ * V = 5
+ * X = 10
+ * L = 50
+ * C = 100
+ * D = 500
+ * M = 1000
+ */
+public class RomanNumeral {
+
+  public static void main(String[] args) {
+    //
+  }
+
+  public static int convert(String romanInput) {
+   return 0;
+  }
+}
